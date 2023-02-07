7e983afe77dcbea2bb66c2f8d3da56d5c0b26ac0
Dika Handika <dika.handika@paper.id>
2023-02-07 07:02:17 +0700

    delete initial lib files

--- projects/my-example-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of my-example-lib
- */
-
-export * from './lib/my-example-lib.service';
-export * from './lib/my-example-lib.component';
-export * from './lib/my-example-lib.module';
